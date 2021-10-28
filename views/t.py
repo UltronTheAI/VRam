972e4c1d20d9e95cdb17a8380043ee7efb16650b
Swaraj Puppalwar <79976106+UltronTheAI@users.noreply.github.com>
2021-10-28 08:10:19 -0700

    Delete t.py

--- views/t.py
+++
@@ -1,8 +0,0 @@
-import requests, json
-
-code = requests.get('http://worldtimeapi.org/api/timezone/Asia/Kolkata')
-
-res = code.text
-res = json.loads(str(res))
-
-print(res['utc_offset'].replace('+', '').replace(':', ',').split(','))
